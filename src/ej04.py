1c0baad7d7d4c538c6a1f293fe5dd341dcdda614
PabloFdez06 <ffpablo@iesrafaelalberti.es>
2024-09-27 13:30:23 +0200

    Ejercicios del 4 al 7

---
+++ src/ej04.py
@@ -0,0 +1,6 @@
+celsius = int(input("Inserte una temperatura en celsius: "))
+
+farenheit = str(celsius * 9 / 5 + 32)
+
+str(print("Esta es la conversión a grados farenheit: " + farenheit + " ºF"))
+
