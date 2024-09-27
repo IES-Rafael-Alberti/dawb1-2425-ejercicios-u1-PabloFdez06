61f4567c0f4fdaa12627e20895523a3a0a1ca3a0
PabloFdez06 <ffpablo@iesrafaelalberti.es.>
2024-09-27 21:32:28 +0200

    Ejercicios del 8 al 12

---
+++ src/ej08.py
@@ -0,0 +1,3 @@
+numeros = int(input("Dime el primer número: ")) + int(input("Dime el segundo número: ")) + int(input("Dime el tercer número: "))
+total = numeros #esta sobra no vale para nada pero pediste 2 variables jaja#
+print("El total de la suma de los 3 números es: " + str(total))
