22a5a51d59e014d142d04fa4ad3f514792f7c8f1
PabloFdez06 <ffpablo@iesrafaelalberti.es.>
2024-10-01 17:38:54 +0200

    Nuevos ejercicios realizados

--- src/ej15.py
+++ src/ej15.py
@@ -1,12 +1,8 @@
 capital = float(input("Intruduce la cantidad que se haya en tu cuenta de ahorros: "))
-interes = float(0.4)
+interes = float(0.04)
 
-capital1 = capital + (1 + interes)
-capital2 = capital1 * (1 + interes)
-capital1 = capital1 + (capital * (1 + interes))
+capital1 = capital + capital * interes
+capital2 = capital1 + capital * interes
+capital3 = capital2 + capital * interes
 
-
-
-print("Tus ahorros son ")
-
-#### INCOMPLETO
+print("Tus ahorros del primer año son ", capital1, "Los del segundo año son", capital2, "y los del tercer año son", capital3,".")
