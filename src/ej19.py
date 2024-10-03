1e25177197ccbeb173d29c825d366d1f6055a11e
PabloFdez06 <ffpablo@iesrafaelalberti.es.>
2024-10-03 21:20:22 +0200

    Nuevos ejercicios.

--- src/ej19.py
+++ src/ej19.py
@@ -2,4 +2,4 @@ nombre = input("Introduce tu nombre: ")
 letras = len(nombre)
 nombre = nombre.upper()
 
-print(f"{nombre} tiene {letras} letras. donde NOMBRE es el nombre de usuario en mayúsculas y n es el número de letras que tienen el nombre.")
+print(f"{nombre} tiene {letras} letras.")
