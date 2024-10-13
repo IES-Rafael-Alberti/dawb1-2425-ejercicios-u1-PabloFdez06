344d872a576d9c7fcf7e65df09de0b8ee8a400f8
PabloFdez06 <ffpablo@iesrafaelalberti.es>
2024-10-13 19:58:38 +0200

    Juego de número aleatorio realizado junto a varios ejercicios más.

--- src/ej25.py
+++ src/ej25.py
@@ -1,4 +1,27 @@
+def introduce_fecha(fecha_nacimiento):
 
+    fecha_nacimiento = fecha_nacimiento.strip()
+
+    dia, mes, año = fecha_nacimiento.split("/")
+
+    dia = int(dia)
+    mes = int(mes)
+
+
+    return dia, mes, año
+
+def main():
+    
+    fecha_nacimiento = input("Introduce tu fecha de nacimiento con el siguiente formato **dd/mm/aaaa**: ")
+    dia, mes, año = introduce_fecha(fecha_nacimiento)
+    
+    print(f"Dia: {dia}, Mes: {mes}, Año: {año}")
+
+
+if __name__ == "__main__":
+    main()
+    
 
 
 # Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año. Adaptar el programa anterior para que también funcione cuando el día o el mes se introduzcan con un solo carácter.
+
